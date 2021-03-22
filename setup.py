aab1ab5139a8c1647a9744f504d89b97c9964c98
wenta <jakubwenta@gmail.com>
2021-03-22 14:42:40 +0100

    add exec back

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='swmm5-api',
-    version='0.4',
+    name='swmm5',
+    version='0.1',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='MIT',
     description='SWMM5 runner',
     long_description=open('README.md').read(),
     install_requires=[''],
-    url='https://github.com/carldata/swmm5-api',
+    url='https://github.com/carldata/swmm5',
     author='Jakub Wenta',
     author_email='jakubwenta@gmail.com'
 )
