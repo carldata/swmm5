40014cafc6ebddfd2a5e23a0e1b5104e3c970220
wenta <jakubwenta@gmail.com>
2021-03-22 10:19:24 +0100

    include_package_data

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='swmm5-api',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
     license='MIT',
     description='SWMM5 runner',
     long_description=open('README.md').read(),
