4b7145a840d26ff6515ec53ff2a8bf457e6d1a17
wenta <jakubwenta@gmail.com>
2021-03-23 11:34:34 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='swmm5',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='MIT',
